37234cdd222f91cae94bab4038a77e037153b2e4
JEFF DARLING <jdarling@medline.com>
2018-09-21 08:59:17 -0500

    Add project files.

---
+++ DemoXSLTransform/Main.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="DemoXSLTransform.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+      
+    <div id="out1" runat="server"></div>
+   
+  
+</body>
+</html>
---
+++ DemoXSLTransform/Main.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Xsl;
+using System.IO;
+using System.Text;  
+
+namespace DemoXSLTransform
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            string xslInput = "data\\cdcatalog.xsl";
+            string xmlInput = "data\\cdcatalog.xml";
+                       
+
+            // Getting file path    
+            string strXSLTFile = Server.MapPath(xslInput);
+            string strXMLFile = Server.MapPath(xmlInput);
+
+            // Creating XSLCompiled object    
+            XslCompiledTransform objXSLTransform = new XslCompiledTransform();
+            objXSLTransform.Load(strXSLTFile);
+
+            // Creating StringBuilder object to hold html data and creates TextWriter object to hold data from XslCompiled.Transform method    
+            StringBuilder htmlOutput = new StringBuilder();
+            TextWriter htmlWriter = new StringWriter(htmlOutput);
+
+            // Creating XmlReader object to read XML content    
+            XmlReader reader = XmlReader.Create(strXMLFile);
+
+            // Call Transform() method to create html string and write in TextWriter object.    
+            objXSLTransform.Transform(reader, null, htmlWriter);
+            this.out1.InnerHtml = htmlOutput.ToString();
+
+            // Closing xmlreader object    
+            reader.Close();    
+
+
+
+        }
+    }
+}
